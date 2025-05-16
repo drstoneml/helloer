2d46fdd18f71f73a15ba3463f18d883ae5c4504f
drstone.ml <manavchavda594@gmail.com>
2025-05-16 15:29:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helloer',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Manav',
+    author_email='manavchavda594@gmail.com',
+    description='A simple greeting package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/drstoneml/helloer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT',
+    python_requires='>=3.6',
+)
