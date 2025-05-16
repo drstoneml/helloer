629d256b45ae984f4d4d61b74e2c0d48f8a7528d
drstone.ml <manavchavda594@gmail.com>
2025-05-16 15:53:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='helloer',
     version='0.1.0',
-    packages=find_packages(),
+    packages=["helloer"],
     install_requires=[],
     author='Manav',
     author_email='manavchavda594@gmail.com',
